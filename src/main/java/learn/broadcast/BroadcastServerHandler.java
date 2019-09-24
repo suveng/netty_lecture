fe271f411fac5b4791af78302a8af2cf3d65dadd
suveng <suveng@163.com>
2019-09-24 20:54:30 +0800

    init broadcast

---
+++ src/main/java/learn/broadcast/BroadcastServerHandler.java
@@ -0,0 +1,24 @@
+package learn.broadcast;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ * description:
+ * @author suwenguang
+ * @version 1.0.0
+ **/
+public class BroadcastServerHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
