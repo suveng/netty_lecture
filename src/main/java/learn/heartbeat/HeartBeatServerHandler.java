32d4592a99d83b49ae2cc424513fd91dcf3e4abe
suveng <suveng@163.com>
2019-10-06 15:58:38 +0800

    心跳包

---
+++ src/main/java/learn/heartbeat/HeartBeatServerHandler.java
@@ -0,0 +1,47 @@
+package learn.heartbeat;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ *
+ * @author suwenguang
+ * @version 1.0.0
+ **/
+public class HeartBeatServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 用户时间触发
+     * @author suwenguang
+     * @date 2019/10/6
+     */
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        //如果event是IdleStateEvent,强转
+        if (evt instanceof IdleStateEvent){
+            IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
+            String eventType = null;
+
+            switch (idleStateEvent.state()){
+                case ALL_IDLE:
+                    eventType="读写空闲";
+                    break;
+                case READER_IDLE:
+                    eventType="读空闲";
+                    break;
+                case WRITER_IDLE:
+                    eventType="写空闲";
+                    break;
+                default:
+
+            }
+            Channel channel = ctx.channel();
+            System.out.println(channel.remoteAddress()+"-"+eventType+",关闭channel");
+            channel.close();
+        }
+    }
+}
---
+++ src/main/java/learn/heartbeat/HeartBeatServerInitializer.java
@@ -0,0 +1,26 @@
+package learn.heartbeat;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author suwenguang
+ * @version 1.0.0
+ **/
+public class HeartBeatServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        //空闲状态监测处理器
+        pipeline.addLast(new IdleStateHandler(5,7,10, TimeUnit.SECONDS));
+        //自定义处理器
+        pipeline.addLast(new HeartBeatServerHandler());
+    }
+}
